937c0f90e00ae0f3618c504305502362c55bc9c2
gfeelixsantos <felix.devx@gmail.com>
2025-12-10 15:45:18 -0300

    10/12/2025 update: include edit feature

--- app/relatorio/page.tsx
+++ app/relatorio/page.tsx
@@ -49,7 +49,7 @@ import { HeaderApp } from "@/components/shared/HeaderApp";
 import { formatCPF, getCurrentUser, getStatusColor, logout } from "@/lib/utils";
 import { useModalOptimizer } from "@/hooks/useModalOptimizer";
 import { useOptimizedDebounce } from "@/hooks/useDebounceOptimizer";
-import { useUser } from "@/hooks/useUser";
+import { IUserInfo, useUser } from "@/hooks/useUser";
 
 // Componente lazy para o conteúdo pesado do modal
 const LazyModalContent = lazy(() => import("./LazyModalContent"));
@@ -88,7 +88,7 @@ interface PaginatedReportData {
 // Componente principal
 export default function RelatoriosPage() {
   const router = useRouter();
-  const userApp = useUser()
+  const [ userApp, setUserApp] = useState<IUserInfo | null>(null)
   const [loading, setLoading] = useState(true);
   const [isFiltering, setIsFiltering] = useState(false);
   const [showResults, setShowResults] = useState(false);
@@ -148,8 +148,10 @@ export default function RelatoriosPage() {
     const currentUser = getCurrentUser();
 
     if (!currentUser) {
-      router.push("/");
+      return router.push("/");
     }
+
+    setUserApp(currentUser)
   }, [router]);
 
   // Função para scroll para o topo da tabela
